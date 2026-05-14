cde8d47f2f284d245e51c9e8f8ca2aa852c2e110
15glucas <gosgnach24lucas@gmail.com>
2026-05-14 21:12:05 +0200

    FR message

---
+++ lang/fr/messages.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'pricing' => [
+        'toggle_monthly' => 'Mensuel',
+        'toggle_yearly' => 'Annuel',
+        'per_year' => '/an',
+        'per_month' => '/mois',
+        'per_user' => 'utilisateur',
+        'recommended' => 'Recommandé',
+        'quantity' => 'Quantité',
+        'installation_fee' => 'Frais d’installation',
+        'one_time' => 'paiement unique',
+    ],
+    'checkout' => [
+        'first_name' => 'Prénom',
+        'last_name' => 'Nom',
+        'company_name' => 'Nom de l’entreprise',
+    ],
+    'buttons' => [
+        'get_started' => 'Commencer',
+        'get_started_free' => 'Commencer gratuitement',
+        'create_account_to_subscribe' => 'Créer un compte pour s’abonner',
+    ],
+];
